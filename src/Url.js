b70019b7ab80661d85332d7c1b88a41e7ee60c63
Vivek V H <90766660+Vivek-v-h@users.noreply.github.com>
2022-05-10 21:22:23 +0530

    Create Url.js

---
+++ src/Url.js
@@ -0,0 +1,13 @@
+import { Api_key } from "./constants/constants";
+export const Originals=`https://api.themoviedb.org/3/discover/tv?api_key=${Api_key}&with_networks=213`
+export const Action=`https://api.themoviedb.org/3/discover/movie?api_key=${Api_key}&with_genres=28`
+
+export const ComedyMovies= `https://api.themoviedb.org/3/discover/movie?api_key=${Api_key}&with_genres=35`
+
+export const HorrorMovies= `https://api.themoviedb.org/3/discover/movie?api_key=${Api_key}&with_genres=27`
+
+export const ActionMovies= `https://api.themoviedb.org/3/discover/movie?api_key=${Api_key}&with_genres=28`
+
+export const RomanceMovies= `https://api.themoviedb.org/3/discover/movie?api_key=${Api_key}&with_genres=10749`
+
+export const Documentaries=`https://api.themoviedb.org/3/discover/movie?api_key=${Api_key}&with_genres=99`
